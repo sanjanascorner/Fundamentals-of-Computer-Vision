ffe97ba8883017c614f1bd032ed5b7b7d2f5a08d
R Sanjana <149611337+sanjanascorner@users.noreply.github.com>
2026-07-16 12:05:37 +0530

    Update comment for Laplacian filter usage

--- marr_hildreth.py
+++ marr_hildreth.py
@@ -2,7 +2,7 @@ import cv2
 img=cv2.imread("image.webp")
 img_blur=cv2.GaussianBlur(img,(3,3),0)
 img_gray=cv2.cvtColor(img_blur,cv2.COLOR_BGR2GRAY)
-img_filtered=cv2.Laplacian(img_gray,cv2.CV_64F,ksize=3) #using built-in image
+img_filtered=cv2.Laplacian(img_gray,cv2.CV_64F,ksize=3) #using built-in function
 cv2.imshow("Original",img)
 cv2.waitKey(0)
 cv2.imshow("Gaussian Blurred",img_blur)
